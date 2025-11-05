442894421655dbde31749f294a61b26380e3751d
Tchlaw <Shantt@gmail.com>
2025-11-05 13:07:41 -0800

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,18 @@
+export default function Head() {
+  return (
+    <>
+      {/* Google tag (gtag.js) */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-0D90N0FQYZ" />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0D90N0FQYZ');
+          `,
+        }}
+      />
+    </>
+  )
+}
