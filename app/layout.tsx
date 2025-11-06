e714848b70b8fe3396fa9a1be112c789371327a6
Tchlaw <Shantt@gmail.com>
2025-11-05 17:08:45 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,15 @@ export default function RootLayout({
       <body className={inter.className}>
 
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-0D90N0FQYZ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-0D90N0FQYZ');
+</script>
 
         
         <ThemeProvider attribute="class" defaultTheme="light">
