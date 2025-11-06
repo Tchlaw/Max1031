e167c5b595ad0bea6d6500d2ca6462abfc575f48
Tchlaw <Shantt@gmail.com>
2025-11-05 17:11:14 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Maximize 1031 | Defer Capital Gains Taxes",
   description: "Maximize 1031 is here to help you defer taxes on your next real estate sale.",
-    generator: 'v0.dev'
+  generator: "v0.dev"
 }
 
 export default function RootLayout({
@@ -22,20 +22,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      
       <body className={inter.className}>
-
-
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-0D90N0FQYZ"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-0D90N0FQYZ');
-</script>
-
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-0D90N0FQYZ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0D90N0FQYZ');
+          `}
+        </Script>
         
         <ThemeProvider attribute="class" defaultTheme="light">
           <Navbar />
